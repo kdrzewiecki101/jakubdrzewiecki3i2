fca3c75e50992c8d6da5782e55c002f11804a104
kdrzewiecki101 <k.drzewiecki101@gmail.com>
2021-12-26 15:29:05 +0100

    newPage

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get("/", function (req, res) {
     res.send("<h1>my app on heroku! - Dalej się bawimy w aplikację Jakuba Drzewieckiego 2021</h1>")
 })
 
+app.get("/data", function (req, res) {
+        res.send("<h1>Kubson to ziomal</h1>")
+    })
+
 app.listen(PORT, function () {
     console.log("start serwera na porcie + mała zmiana w pliku!" + PORT)
 })
