81ccfac41151ae4e932f4a8cee7669197ccb8ff9
kdrzewiecki101 <k.drzewiecki101@gmail.com>
2022-01-06 23:21:30 +0100

    firstView

--- server.js
+++ server.js
@@ -3,13 +3,9 @@ const app = express()
 const PORT = process.env.PORT || 3000; // bardzo istotna linijka - port zostaje przydzielony przez Heroku
 
 app.get("/", function (req, res) {
-    res.send("<h1>my app on heroku! - Dalej się bawimy w aplikację Jakuba Drzewieckiego 2021</h1>")
+    res.send("<h1>my app on heroku!</h1>")
 })
 
-app.get("/data", function (req, res) {
-        res.send("<h1>Kubson to ziomal</h1>")
-    })
-
 app.listen(PORT, function () {
-    console.log("start serwera na porcie + mała zmiana w pliku!" + PORT)
+    console.log("start serwera na porcie " + PORT)
 })
