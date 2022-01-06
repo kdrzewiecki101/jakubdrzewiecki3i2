e955ea8b69fa079f1d08547aff903119854840f9
kdrzewiecki101 <k.drzewiecki101@gmail.com>
2022-01-06 23:24:39 +0100

    newObject

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
+let dane = {
+    imie: "Jakub",
+    nazwisko: "Drzewiecki",
+    klasa: "3I2",
+    grupa: "b"
+}
+
 const express = require("express")
 const app = express()
 const PORT = process.env.PORT || 3000; // bardzo istotna linijka - port zostaje przydzielony przez Heroku
 
 app.get("/", function (req, res) {
-    res.send("<h1>my app on heroku!</h1>")
+    res.send(dane)
 })
 
 app.listen(PORT, function () {
